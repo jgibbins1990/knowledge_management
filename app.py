e3198eb2021069c73a735812cdbbd521134815da
James Gibbins <69259638+jgibbins1990@users.noreply.github.com>
2024-03-01 14:30:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st 
 import pickle
-import sklearn
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
 
 model = pickle.load(open('model.pkl', 'rb'))
 cols=['combined']    
