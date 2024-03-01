1b6f1e43b91f1a6ab55af76996dfaa6b84491a21
James Gibbins <69259638+jgibbins1990@users.noreply.github.com>
2024-03-01 14:27:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st 
 import pickle
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
